bea426a3880ed53471d84d179ed466450256da9a
Victor Oliveira <vamo89@gmail.com>
2021-10-02 16:30:52 -0300

    Using our strapi-mongo graphql cms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,33 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
-const Home: NextPage = () => {
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: "https://whispering-stream-54419.herokuapp.com/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  const { data } = await client.query({
+    query: gql`
+      query Categories {
+        categories {
+          name
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      categories: data.categories,
+    },
+  };
+}
+
+const Home: NextPage = (props: { categories: any }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,43 +37,23 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Bem vindo à nossa loja virtual!</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <p className={styles.description}>Comida de verdade!</p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {props.categories.map((category: any) => {
+            return (
+              <a
+                key={category.name}
+                href={`/${category.name}`}
+                className={styles.card}
+              >
+                <h2>{category.name}</h2>
+                <p>More about it....</p>
+              </a>
+            );
+          })}
         </div>
       </main>
 
@@ -59,14 +63,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
