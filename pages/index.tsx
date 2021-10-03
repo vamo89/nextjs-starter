ede0287084392f8de96cd8838d70d7e93534316a
Victor Oliveira <vamo89@gmail.com>
2021-10-03 16:28:41 -0300

    Creating distribution center page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,99 +1,15 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import type { GetServerSidePropsContext, NextPage } from "next";
 
-type Props = {
-  categories: [
-    {
-      name: string;
-      slug: string;
-      image: {
-        url: string;
-      };
-    }
-  ];
-};
-
-export async function getStaticProps() {
-  const client = new ApolloClient({
-    uri: "https://whispering-stream-54419.herokuapp.com/graphql",
-    cache: new InMemoryCache(),
-  });
-
-  const { data } = await client.query({
-    query: gql`
-      query Categories {
-        categories {
-          name
-          slug
-          image {
-            url
-          }
-        }
-      }
-    `,
-  });
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  console.log(context.req.headers);
 
   return {
-    props: {
-      categories: data.categories,
+    redirect: {
+      destination: "/sp",
+      permanent: false,
     },
-    revalidate: 60,
   };
 }
 
-const Home: NextPage<Props> = (props: Props) => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Comida Virtual</title>
-        <meta name="description" content="Sua loja de comida virtual" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Bem vindo!</h1>
-
-        <p className={styles.description}>A primeira loja de comida virtual</p>
-
-        <div className={styles.grid}>
-          {props.categories.map((category) => {
-            return (
-              <a
-                key={category.name}
-                href={`/categories/${category.slug}`}
-                className={styles.card}
-              >
-                <Image
-                  src={`${category.image.url}`}
-                  alt={`Foto da categoria ${category.name}`}
-                  width={310}
-                  height={200}
-                />
-                <h2>{category.name}</h2>
-                <p>Esta é a categoria de {category.name}</p>
-              </a>
-            );
-          })}
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  );
-};
-
+const Home: NextPage = () => null;
 export default Home;
