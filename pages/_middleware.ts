1aca85a3b0f01460ca9aa7dbf0664652df10cc0b
Victor Oliveira <vamo89@gmail.com>
2021-10-28 15:54:11 -0300

    Find DC by IP as fallback

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,12 +1,26 @@
 import { NextResponse, NextRequest } from "next/server";
+import findDistributionCenterByLocation from "../helpers/findDistributionCenterByLocation";
+import findLocationByIp from "../helpers/findLocationByIp";
 
 const acceptedRegions = ["mg", "sp", "rj"];
 
-export default function middleware(request: NextRequest) {
+export default async function middleware(request: NextRequest) {
   const url = request.nextUrl;
   let response = NextResponse.next();
   if (url.pathname == "/") {
     const dc = request.cookies["distribution_center"];
+    const location = request.ip && findLocationByIp(request.ip);
+    const dcByIp = location && findDistributionCenterByLocation(await location);
+    console.log(
+      "cookie: ",
+      dc,
+      "geo: ",
+      request.geo,
+      "ip: ",
+      request.ip,
+      "dc by ip: ",
+      dcByIp
+    );
     if (dc) {
       response = NextResponse.rewrite(`/${dc}`);
     } else if (
@@ -15,9 +29,13 @@ export default function middleware(request: NextRequest) {
       request.geo.region &&
       acceptedRegions.includes(request.geo.region.toLowerCase())
     ) {
-      const new_dc = request.geo.region.toLowerCase();
-      response.cookie("distribution_center", new_dc);
-      response = NextResponse.rewrite(`/${new_dc}`);
+      // Only available in Vercel's pro or enterprise plans
+      const newDc = request.geo.region.toLowerCase();
+      response.cookie("distribution_center", newDc);
+      response = NextResponse.rewrite(`/${newDc}`);
+    } else if (dcByIp) {
+      response.cookie("distribution_center", dcByIp);
+      response = NextResponse.rewrite(`/${dcByIp}`);
     } else {
       response = NextResponse.rewrite(`/sp`);
     }
